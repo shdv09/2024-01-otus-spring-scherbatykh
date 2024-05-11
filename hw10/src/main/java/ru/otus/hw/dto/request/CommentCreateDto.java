cc4e1d151a86a6b607a560dacd69a70fa4beb3f0
Scherbatykh Denis <DScherbatykh@sportmaster.ru>
2024-05-11 15:16:28 +0300

    tests

--- hw10/src/main/java/ru/otus/hw/dto/request/CommentCreateDto.java
+++ hw10/src/main/java/ru/otus/hw/dto/request/CommentCreateDto.java
@@ -10,9 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class CommentCreateDto {
-
-    private long id;
-
     @NotBlank(message = "Comment text should not be blank")
     @Size(min = 2, max = 20, message = "Comment text should have expected size")
     private String text;
