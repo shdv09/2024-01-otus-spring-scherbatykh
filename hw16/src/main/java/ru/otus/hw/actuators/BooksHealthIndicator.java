44993d09152288929cb7eee27a33acbfa9a2e633
Scherbatykh Denis <DScherbatykh@sportmaster.ru>
2024-06-19 10:13:25 +0300

    fixes after review

--- hw16/src/main/java/ru/otus/hw/actuators/BooksHealthIndicator.java
+++ hw16/src/main/java/ru/otus/hw/actuators/BooksHealthIndicator.java
@@ -3,26 +3,35 @@ package ru.otus.hw.actuators;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.repositories.BookRepository;
 
 @RequiredArgsConstructor
 @Component
 public class BooksHealthIndicator implements HealthIndicator {
+    private static final Health STATUS_DOWN = Health.down()
+            .withDetail("message", "Book storage is empty")
+            .build();
+
+    private static final Health STATUS_UP = Health.up()
+            .withDetail("message", "Everything is good, so far)")
+            .build();
 
     private final BookRepository bookRepository;
 
     @Override
     public Health health() {
-        boolean storageEmpty = bookRepository.count() == 0;
+        boolean storageEmpty;
+        try {
+            storageEmpty = bookRepository.count() == 0;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return STATUS_DOWN;
+        }
         if (storageEmpty) {
-            return Health.down()
-                    .status(Status.DOWN)
-                    .withDetail("message", "Book storage is empty")
-                    .build();
+            return STATUS_DOWN;
         } else {
-            return Health.up().withDetail("message", "Everything is good, so far)").build();
+            return STATUS_UP;
         }
     }
 }
