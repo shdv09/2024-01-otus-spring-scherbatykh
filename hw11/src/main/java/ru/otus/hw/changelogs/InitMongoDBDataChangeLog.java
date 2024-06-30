c0cab3b5243054f63f76ad0ddcff908d6fd2da5b
Scherbatykh Denis <DScherbatykh@sportmaster.ru>
2024-06-30 18:40:35 +0300

    fix mongock

--- hw11/src/main/java/ru/otus/hw/changelogs/InitMongoDBDataChangeLog.java
+++ hw11/src/main/java/ru/otus/hw/changelogs/InitMongoDBDataChangeLog.java
@@ -2,15 +2,12 @@ package ru.otus.hw.changelogs;
 
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
 import com.mongodb.client.MongoDatabase;
 import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Comment;
 import ru.otus.hw.models.Genre;
-import ru.otus.hw.repositories.AuthorRepository;
-import ru.otus.hw.repositories.BookRepository;
-import ru.otus.hw.repositories.CommentRepository;
-import ru.otus.hw.repositories.GenreRepository;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -30,37 +27,37 @@ public class InitMongoDBDataChangeLog {
     }
 
     @ChangeSet(order = "001", id = "initAuthors", author = "shdv09", runAlways = true)
-    public void initAuthors(AuthorRepository repository) {
+    public void initAuthors(MongockTemplate mongockTemplate) {
         for (int i = 0; i < 3; i++) {
             Author author = new Author(null, "author" + i);
-            repository.save(author).subscribe();
+            mongockTemplate.save(author);
             authors.add(author);
         }
     }
 
     @ChangeSet(order = "002", id = "initGenres", author = "shdv09", runAlways = true)
-    public void initGenres(GenreRepository repository) {
+    public void initGenres(MongockTemplate mongockTemplate) {
         for (int i = 0; i < 6; i++) {
             Genre genre = new Genre(null, "genre" + i);
-            repository.save(genre).subscribe();
+            mongockTemplate.save(genre);
             genres.add(genre);
         }
     }
 
     @ChangeSet(order = "003", id = "initBooks", author = "shdv09", runAlways = true)
-    public void initBooks(BookRepository repository) {
+    public void initBooks(MongockTemplate mongockTemplate) {
         for (int i = 0; i < 3; i++) {
             Book book = new Book(null, "book" + i, authors.get(i), genres.subList(i, i + 2));
-            repository.save(book).subscribe();
+            mongockTemplate.save(book);
             books.add(book);
         }
     }
 
     @ChangeSet(order = "004", id = "initComments", author = "shdv09", runAlways = true)
-    public void initComments(CommentRepository repository) {
+    public void initComments(MongockTemplate mongockTemplate) {
         for (int i = 0; i < 3; i++) {
             Comment comment = new Comment(null, "comment" + i, books.get(0));
-            repository.save(comment).subscribe();
+            mongockTemplate.save(comment);
         }
     }
 }
