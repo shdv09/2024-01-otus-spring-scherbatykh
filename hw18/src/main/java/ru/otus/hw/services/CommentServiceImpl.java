45aa6dcc3655d7ddfd0b552898163f2fcf1e808e
Scherbatykh Denis <DScherbatykh@sportmaster.ru>
2024-07-11 19:44:33 +0300

    circuit breaker

---
+++ hw18/src/main/java/ru/otus/hw/services/CommentServiceImpl.java
@@ -0,0 +1,53 @@
+package ru.otus.hw.services;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.dto.response.CommentDto;
+import ru.otus.hw.dto.mappers.CommentMapper;
+import ru.otus.hw.exceptions.NotFoundException;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Comment;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CircuitBreaker(name = "daoCircuitBreaker")
+@RequiredArgsConstructor
+@Service
+public class CommentServiceImpl implements CommentService {
+    private final BookRepository bookRepository;
+
+    private final CommentRepository commentRepository;
+
+    private final CommentMapper commentMapper;
+
+    @Transactional
+    @Override
+    public CommentDto create(long bookId, String commentText) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new NotFoundException("Book with id = %d not found".formatted(bookId)));
+        Comment comment = new Comment();
+        comment.setText(commentText);
+        comment.setBook(book);
+        return commentMapper.toDto(commentRepository.save(comment));
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public CommentDto findById(long id) {
+        return commentRepository.findById(id)
+                .map(commentMapper::toDto)
+                .orElseThrow(() -> new NotFoundException("Comment with id = %d not found".formatted(id)));
+    }
+
+    @Override
+    public List<CommentDto> findByBookId(long bookId) {
+        return commentRepository.findByBookId(bookId).stream()
+                .map(commentMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
